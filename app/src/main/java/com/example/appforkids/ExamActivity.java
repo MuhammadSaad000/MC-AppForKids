48d5eb6409fd4046da9f5e6a0380babab44e2288
Muhammad Saad <muhammadsaad6345@gmail.com>
2022-05-23 14:45:00 +0500

    New Activity Added

---
+++ app/src/main/java/com/example/appforkids/ExamActivity.java
@@ -0,0 +1,13 @@
+package com.example.appforkids;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam);
+    }
+}
--- app/src/main/java/com/example/appforkids/MainActivity.java
+++ app/src/main/java/com/example/appforkids/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.appforkids;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +24,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button repo;
+        repo = findViewById(R.id.repo);
+        repo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://github.com/MuhammadSaad000/MC-AppForKids.git");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+});
 
     }
 }
